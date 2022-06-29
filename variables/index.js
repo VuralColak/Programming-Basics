ce91205003de4fa006a19d1699a6f3713b5c77e9
Vural CoLak <vuralcolakcontact@gmail.com>
2022-06-29 11:48:14 +0200

    console.log und data types gelernt

---
+++ variables/index.js
@@ -0,0 +1,12 @@
+console.log ("Hello, Welcome to My First JavaScript File...")
+
+
+const essen1 = "Pilav"
+const essen2 = "Adana Kebap"
+const essen3 = "Beyti"
+const essen4 = "Döner"
+const essen5 = "Pirzola"
+
+console.log("My Favorite Food is " + essen3)
+
+console.log(essen3, typeof essen3)
