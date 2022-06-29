69e92abc16656590c39d7e40531e4b47eb8ab4fb
Vural CoLak <vuralcolakcontact@gmail.com>
2022-06-29 13:49:12 +0200

    first aufgabe fertig

---
+++ aufgaben/aufgabe1.js
@@ -0,0 +1,36 @@
+//aufgabe1-1 start
+
+
+const vorname = "Vural";
+const land = "Türkei";
+const essen1 = "Beyti";
+const essen2 = "Pirzola";
+const alter = 30;
+const geburtsJahr = 1991;
+
+
+console.log("Hallo ich bin " + vorname + " und ich komme aus " + land + ".");
+
+console.log("Meine Lieblingsspeisen sind " + essen1 + " und " + essen2 +".");
+
+console.log("Ich bin " + alter + " Jahre alt und " + geburtsJahr + " geboren.");
+
+
+
+//aufgabe1-1 end
+
+
+
+
+
+//aufgabe1-2 start
+
+
+
+
+
+
+
+
+
+//aufgabe1-2 end
