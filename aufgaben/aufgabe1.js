6d9c971c6c2eaa32f25329705523451b7709b3d3
Vural CoLak <vuralcolakcontact@gmail.com>
2022-06-29 14:40:16 +0200

    aufgaben fertig

--- aufgaben/aufgabe1.js
+++ aufgaben/aufgabe1.js
@@ -23,14 +23,97 @@ console.log("Ich bin " + alter + " Jahre alt und " + geburtsJahr + " geboren.");
 
 
 
-//aufgabe1-2 start
+//aufgabeA start
+
+
+const meter = 100;
+const schwimmbecken = 3 * meter;
+const enthaelt = 3750;
+const kostengründen = schwimmbecken - 5;
+
+const wettkämpfe = (enthaelt * kostengründen) / schwimmbecken;
+
+console.log("Aufgabe A/ Wasser = " + wettkämpfe + " m3.")
+
+
+
+//aufgabeA end
 
 
 
 
+//aufgabeB start
+
+
+const verbrauch = 8;
+const weg = 100;
+const tankfüllung = 66;
+
+
+const weit = (tankfüllung * weg) / verbrauch;
+
+console.log("Aufgabe B/ Das Auto geht = " + weit + " km weiter.")
 
 
 
+//aufgabeB end
 
 
-//aufgabe1-2 end
+//aufgabeC start
+
+const arbeiter1 = 3;
+const arbeiter2 = 5;
+const lange1 = 17;
+
+
+const lange2 = arbeiter1 * lange1 / arbeiter2;
+
+console.log("Aufgabe C/ Es dauert = " + lange2 + " Stunde.")
+
+//aufgabeC end
+
+//aufgabeD start
+
+const anzahl1 = 882;
+const anzahl2 = 14;
+const anzahl3 = 12;
+const anzahl4 = 2.3;
+const anzahl5 = 999;
+
+
+const gesamt = anzahl1 / anzahl2 * anzahl3 / anzahl4 * anzahl5;
+
+console.log("Aufgabe D/ Insgesamt = " + gesamt)
+
+//aufgabeD end
+
+//aufgabeE start
+
+let anzahl6 = 12;
+let anzahl7 = 7;
+let anzahl8 = 17;
+let anzahl9 = 7;
+let anzahl10 = 17;
+let anzahl11 = 333;
+
+
+let gesamt2 = (anzahl6 * anzahl7 * anzahl8 / anzahl9 / anzahl10) + anzahl11;
+
+console.log("Aufgabe E/ Insgesamt = " + gesamt2);
+
+
+//aufgabeE end
+
+//aufgabeF start
+
+const grundstücksgrenze = (16 * 100) + 80;
+const sockel = 120;
+const sockelKosten = 24.40;
+
+
+const geld = (grundstücksgrenze / sockel) * sockelKosten;
+
+console.log("Aufgabe F/ Das kostet = " + geld + " Euro.")
+
+
+//aufgabeF end
