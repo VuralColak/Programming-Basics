094a18a02fccc1dc3c424e161c06ce32fe3ae0c0
Vural CoLak <vuralcolakcontact@gmail.com>
2022-07-04 10:35:42 +0200

    versuch

--- Introduction/nodejs/index.js
+++ Introduction/nodejs/index.js
@@ -1,52 +1,54 @@
+console.time("debug");
+
 const a = 1;
 
 if (a == true) {
-    console.log ("hallo");
+    console.log ("halloa");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!a");
 }
 
 
 const b = null;
 
 if (b == true) {
-    console.log ("hallo");
+    console.log ("hallob");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!b");
 }
 
 
 const c = "abc";
 
 if (c == true) {
-    console.log ("hallo");
+    console.log ("halloc");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!c");
 }
 
 const d = "";
 
 if (d == true) {
-    console.log ("hallo");
+    console.log ("hallod");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!d");
 }
 
 
 const e = "abc";
 
 if (e) {
-    console.log ("hallo");
+    console.log ("halloe");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!e");
 }
 
 const f = "";
 
 if (f == true) {
-    console.log ("hallo");
+    console.log ("hallof");
 } else {
-    console.log("tschüss!");
+    console.log("tschüss!f");
 }
 
 // 12 == "12" => true
@@ -59,3 +61,20 @@ if (!!g == true) {
 } else {
     console.log("tschüss!g");
 }
+
+
+const h = 1;
+
+if (h == true) {
+    console.log ("halloh1");
+} else {
+    console.log("tschüss!h1");
+}
+
+if (h === true) {
+    console.log ("halloh2");
+} else {
+    console.log("tschüss!h2");
+}
+
+console.timeEnd("debug");
