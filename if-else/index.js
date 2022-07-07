566ee2210ca778bfd5eb094fc7de7ae1dfcb9f13
Vural CoLak <vuralcolakcontact@gmail.com>
2022-07-07 14:14:04 +0200

    and or

--- if-else/index.js
+++ if-else/index.js
@@ -65,33 +65,58 @@ if (alterMarie >= 18 || alterJonas >= 18)
 // Ternary = Ein kurzes if, else statement auf einer Zeile.
 
 
-const alterJonas1 = 18;
+// const alterJonas1 = 18;
 
-alterJonas1 >= 18
-    ? console.log("Du darfst Vodka kaufen")
-    : console.log("Du darfst Saft kaufen");
-
-
-
-const verkäuferAntwort =
-    alterJonas1 >= 18 ? "Du darfst Vodka kaufen" : "Du darfst Saft kaufen";
-
-console.log(verkäuferAntwort);
+// alterJonas1 >= 18
+//     ? console.log("Du darfst Vodka kaufen")
+//     : console.log("Du darfst Saft kaufen");
 
 
+// const verkäuferAntwort =
+//     alterJonas1 >= 18 ? "Du darfst Vodka kaufen" : "Du darfst Saft kaufen";
+
+// console.log(verkäuferAntwort);
 
 
-
-const istJonaszwischen16Und21 = alterJonas1 >= 16 && alterJonas1 < 21;
+// const istJonaszwischen16Und21 = alterJonas1 >= 16 && alterJonas1 < 21;
 
 
 
+/**
+ * Ternary
+ *
+ * Was ist eine Ternary?
+ * Ein kurzes if, else statement auf einer Zeile.
+ *
+ * Eine Ternary ist so aufgebaut:
+ *
+ * bedingung ? antwort falls wahr : antwort falls falsch
+ */
 
+ const alterJonas1 = 48;
 
+ alterJonas1 >= 18
+     ? console.log("Du darfst Vodka kaufen")
+     : console.log("Du darfst Saft kaufen");
+ 
+ /**
+  * Aber eine Ternary kann noch mehr:
+  * Wird eine Ternary ausgeführt, bleibt am Ende die Antwort stehen.
+  */
+ 
+ const verkäuferAntwort =
+     alterJonas >= 18 ? "Du darfst Vodka kaufen" : "Du darfst Saft kaufen";
+ 
+ console.log(verkäuferAntwort);
+ 
+ const istJonasZwischen16Und21 = alterJonas >= 16 && alterJonas <= 21;
+ console.log(
+     istJonasZwischen16Und21 ? "Ja er ist dazwischen" : "Nein ist er nicht"
+ );
+ 
+ 
 
-
-
-
+ 
 // Varablen mit dem Or|| Operator zuweisen
 
 const value1 = null;
