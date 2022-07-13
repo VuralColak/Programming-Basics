77b61f328c720e24ce380706ab9b63f24836f3cf
Vural CoLak <vuralcolakcontact@gmail.com>
2022-07-13 10:59:15 +0200

    aufgabe137

--- aufgaben/aufgabe137/index.js
+++ aufgaben/aufgabe137/index.js
@@ -1,17 +1,20 @@
 // Deklariere eine Funktion mit dem Namen istGeradeZahl, welche einen Parameter zahl erhält und einen Boolean zurück gibt. Der Boolean soll true sein wenn die Zahl gerade ist und false wenn sie nicht gerade ist. Führe zum Schluss den folgenden Code aus und überprüfe dein Ergebnis:
 
 
+// weg 1 :
 
+// function istGeradeZahl(zahl){
+
+//     const gerade = zahl % 2 === 0;
+//     return gerade;
+// }
+
+// weg 2 :
 
 function istGeradeZahl(zahl){
 
-    const gerade = zahl % 2 === 0;
-    // if (gerade === true){
-    //     console.log("True");
-    // } else {
-    //     console.log("False");
-    // };
-    return gerade;
+    return zahl % 2 === 0;
+    
 }
 
 console.log(istGeradeZahl(3),"soll false sein");
