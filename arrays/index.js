3e40f49cfca138ad84828d739df6170ea254481c
Vural CoLak <vuralcolakcontact@gmail.com>
2022-07-17 19:15:37 +0200

    letzte commit

--- arrays/index.js
+++ arrays/index.js
@@ -1,3 +1,35 @@
+/**
+ * Arrays
+ *
+ * Arrays sind Datentypen in denen man mehrere andere Datentypen speichern kann
+ *
+ * Vergleichbar ist ein Array mit einem Zug
+ * Und das kommt auch nicht von irgendwo her, es sieht nämlich ein bisschen so aus. Mit Fantasie:
+ *
+ * ["lok", "getreide","autos","benzin"]
+ *
+ * Wie man sieht erstellt man Arrays mit eckigen Klammern. Und die Elemente darin sind mit Kommas getrennt.
+ */
+ 
+ /**
+  * Wozu brauch man Arrays?
+  * Es gibt mehrere Gründe
+  *
+  * - Man hat viele Daten. Zum Beispiel hunderte Instagram Follower die man effizient übertragen und verarbeiten will
+  *
+  * Beispiel: Einen Zug kann man einfach von vorne nach hinten abgehen und sieht dann was er transportiert.
+  *
+  * - Man hat eine unbekannte anzahl an Daten. Der Vorteil vom Array gegenüber einfachen Variablen ist: Man kann einfach Elemente anhängen oder wieder wegnehmen
+  *
+  * Info: Die größten zwei "Unbekannten-Verursacher" beim Programmieren sind Server-Abfragen und Nutzereingaben
+  *
+  * Beispiel: Ein Zug kann eine unbekannte anzahl an Waggons haben.
+  * Eine Woche fährt der ICE733 mit 8 Waggons und in der nächsten nur mit 6. Es ist und bleibt aber der ICE733
+  *
+  *
+  */
+ 
+
 const zug = ["lok", "getreide", "autos", "benzin"];
 console.log(zug);
 
@@ -62,7 +94,7 @@ const sayi3 = [2, 5, 3, 11, 17, 7, 1, 19];
 
 const sayi2 = [5, 7, 3, 12, 16, 9, 1, 21];
 sayi2.reverse();
-console.log({sayi2});
+console.log({sayi2}); // { sayi2: [21, 1, 9, 16, 12, 3, 7,  5] }
 
 
 const langerAnzahl = sayi2.concat(sayi3);
