798ecabf07be790ab8cb8f029d14bb5a5748fe28
Vural CoLak <vuralcolakcontact@gmail.com>
2022-08-31 11:45:55 +0200

    aufgabe318

--- aufgaben/318aufgabe/solutions.js
+++ aufgaben/318aufgabe/solutions.js
@@ -36,3 +36,41 @@ console.log(' ');
 console.log('----Aufgabe-2--------');
 console.log(' ');
 
+
+
+
+// Deine Aufgabe ist es, eine Kreis Klasse zu erstellen. Die konstruierten Kreise müssen zwei Methoden haben: getArea() (PI*r^2) und getPerimeter() (2*PI\*r), die sowohl die jeweilige Fläche als auch den Umfang angeben.
+
+// ## Examples
+
+// ```js
+// let circy = new Circle(11);
+// circy.getArea(); // Should return 380.132711084365
+
+// let circy = new Circle(4.44);
+// circy.getPerimeter(); // Should return 27.897342763877365
+
+
+class Circle {
+
+    constructor(radius) {
+        this.radius = radius; 
+    }
+
+    getArea(){
+        return Math.PI*this.radius*this.radius;
+
+    };
+
+    getPerimeter(){
+        return 2*Math.PI*this.radius 
+    }
+
+};
+
+
+let circy1 = new Circle(11);
+console.log(circy1.getArea()); // Should return 380.132711084365
+
+let circy2 = new Circle(4.44);
+console.log(circy2.getPerimeter()); // Should return 27.897342763877365
