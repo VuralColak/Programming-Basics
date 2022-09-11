f7b99f9500988cef7b91b492e688183c4b148eba
Vural CoLak <vuralcolakcontact@gmail.com>
2022-09-11 15:31:51 +0200

    big-o

--- arrays/sort.js
+++ arrays/sort.js
@@ -96,3 +96,22 @@ const products = [{
 console.log(products.sort((a, b) => {
     return a.product > b.product ? 1 : -1;
 }))
+
+
+
+
+console.log(' ');
+console.log('*****big-o*****');
+console.log(' ');
+
+const zahlen = [1, 7, 2, 8, 3, 4, 5, 0, 9];
+
+for (let i = 1; i < zahlen.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (zahlen[i] < zahlen[j]) {
+            let x = zahlen[i];
+            zahlen[i] = zahlen[j];
+            zahlen[j] = x;
+        }
+    }
+}
