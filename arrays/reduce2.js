89ce91ac6dca9a8c38548754a9c1c4d1e45cd3c4
Vural CoLak <vuralcolakcontact@gmail.com>
2022-09-11 17:49:40 +0200

    reduce-methode

--- arrays/reduce2.js
+++ arrays/reduce2.js
@@ -84,4 +84,115 @@ console.log('**********');
 // array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
 
 
+let shoppingCart = [
+    {
+      product: 'phone',
+      qty: 1,
+      price: 500,
+    },
+    {
+      product: 'Screen Protector',
+      qty: 1,
+      price: 10,
+    },
+    {
+      product: 'Memory Card',
+      qty: 2,
+      price: 20,
+    },
+];
+
+
+let total = shoppingCart.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue.qty * currentValue.price;
+}, 0);
+
+console.log({total});
+
+
+
+console.log('**********');
+
+
+
+// Finding an Average with the Reduce Method In JavaScript​
+
+
+const euros = [29.76, 41.85, 46.5];
+
+const average = euros.reduce((total, amount, index, array) => {
+  total += amount;
+  if( index === array.length-1) { 
+    return total/array.length;
+  }else { 
+    return total;
+  }
+});
+
+console.log({average}); // 39.37
+
+
+
+console.log('**********');
+
+
+
+const average2 = euros.reduce((total, amount, index, array) => {
+    total += amount
+    return total/array.length
+}, 0);
+
+
+console.log({average2}); // 21.252222222222223
+
+
+
+
+console.log('**********');
+
+
+const euros2 = [29.76, 41.85, 46.5];
+
+const doubled = euros2.reduce((total, amount) => {
+  total.push(amount * 2);
+  return total;
+}, []);
+
+console.log({doubled}); // [59.52, 83.7, 93]
+
+
+console.log('**********');
+
+
+
+const euro = [29.76, 41.85, 46.5];
+
+const above30 = euro.reduce((total, amount) => {
+  if (amount > 30) {
+    total.push(amount);
+  }
+  return total;
+}, []);
+
+console.log({above30}); // [ 41.85, 46.5 ]
+
+
+
+console.log('**********');
+
+
+
+
+const fruitBasket = ['banana', 'cherry', 'orange', 'apple', 'cherry', 'orange', 'apple', 'banana', 'cherry', 'orange', 'fig' ];
+
+const count = fruitBasket.reduce( (tally, fruit) => {
+  tally[fruit] = (tally[fruit] || 0) + 1 ;
+  return tally;
+} , {})
+
+console.log({count}); // { banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1 }
+
+
+
+// devami bu sitede; https://www.freecodecamp.org/news/reduce-f47a7da511a9/
 
